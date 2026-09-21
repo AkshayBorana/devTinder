72ba97f1bf0efc0f4608421fb38db8003933cd0c
AkshayBorana <akshayborana1@gmail.com>
2026-09-21 19:30:29 -0400

    Implemented signup API to save a new user.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
+/**
+ * Create a user signup API, to save new user's to database.
+ */
+app.post("/signup", async (req, res) => {
+  const newUser = {
+    firstName: "Virat",
+    lastName: "Kohli",
+    emailId: "viratkohli@gmail.com",
+    password: "12345",
+  };
+  const user = new User(newUser);
+
+  try {
+    await user.save();
+    res.send("User added successfully!");
+  } catch (error) {
+    res.status(400).send(`Error saving the User ${error.message}`);
+  }
+});
 
 /**
  * 1. Connect to the Db first and then listen to the server.
