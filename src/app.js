c61170f91429da6925dc502d311a312bdc1cda75
AkshayBorana <akshayborana1@gmail.com>
2026-09-21 21:47:35 -0400

    Implemented GET /feed API to get all users.

--- src/app.js
+++ src/app.js
@@ -22,14 +22,29 @@ app.post("/signup", async (req, res) => {
 });
 
 /**
+ * Feed API - GET /feed Get all the users from the database.
+ */
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if(!users.length) {
+      res.status(404).send('Users not found');
+    }
+    res.status(200).send(users)
+  } catch (error) {
+    res.status(400).send(`Something went wrong`)
+  }
+})
+
+/**
  * 1. Connect to the Db first and then listen to the server.
  */
 connectDB()
   .then(() => {
-    console.log(`Databse connected successfully!!!!!`);
+    console.log(`Database connected successfully!!!!!`);
     // Server is listening on port 3000;
     app.listen(3000, () => {
-      console.log("Serveer is listening on port: 3000");
+      console.log("Server is listening on port: 3000");
     });
   })
   .catch((error) => {
